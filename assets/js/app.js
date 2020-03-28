b1c13cc33ce3525d62278d37891477287ca6fd1d
najlabenchelly <chelly.n103@gmail.com>
2020-03-28 19:22:06 +0100

    mise en place react

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,17 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import React from "react";
+import ReactDOM from "react-dom";
+require("../css/app.css");
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log("Hello !!!");
+
+const App = () => {
+  return <h1>Coucou tout le monde </h1>;
+};
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App />, rootElement);
